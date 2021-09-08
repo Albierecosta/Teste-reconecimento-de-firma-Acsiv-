4eb849d929005b150357dd7eb4d5c40ce730006f
Albiere <85261094+Albieregit@users.noreply.github.com>
2021-09-08 11:00:13 -0300

    Update acs.py
    Agora e possível salvar as informações em csv e também em txt

--- acs.py
+++ acs.py
@@ -1,48 +1,128 @@
+from time import sleep
 from datetime import date
 today = date.today()
 
-# dd/mm/YY
-dta = today.strftime("%d/%m/%Y")
-dataat = str(input('Data emissao: (caso for a data atual precione a tecla (x))'))
-if dataat == 'x':
-    dataat2 = dta
-else:
-    dataat2 = dataat    
+cidade = str(input('informe sua cidade: \n'))
 
-#dta = str(input('Digite a data da emissao: '))
-ins = 'isento'
-isento = str(input('Qual valor do emolumento? (se for isento digite ins) '))
+arrayEmol = 'Emolumentos: '
 
-if isento == 'ins':
-    isento2 = ins
-else:
-    isento2 = isento
+print("\n---------------------------------------------")
+print("Digite (1) para buscar a data atual.")
+print("Digite (2) para digitar uma data valida.")
+print("----------------------------------------------\n")
+data = int(input(""))
+if data <= 0:
+     print("Opção invalida.")
+     data = int(input("informe uma opção valida: "))
+elif data > 2:
+    print("Opção invalida.")
+    data = int(input("informe uma opção valida: "))
+elif data == 1:
+    print("Buscando data atual...")
+    sleep(3)  
 
-s = 'Semelhança'
-a = 'Autenticidade'
-tipo = str(input('Digite (s) para semelhança, (a) para autenticidade: '))
-if tipo == 's':
-   tipo2 = s
-else:
-    tipo2 = a
-qtp = int(input('Quantas pessoas são? '))
-pessoas = list()
+def opcao1():
+    global dataAtual
+    dataAtual = today.strftime("%d/%m/%Y")
 
-for i in range(qtp):
-    nome = input('Nome: ')
-    cpf = input('Cpf: ')
-    pessoas.append([nome, cpf])
-    
-quantidade = int(input('Quantidade de etiquetas? '))
+def opcao2():
+    global dataAtual
+    dataAtual = str(input("Informe uma data valida: "))
 
-#print(pessoas, tipo2, insento2, dta,)
+opcoes = { 1:opcao1, 2:opcao2}
+opcoes.get(data)()
 
-for x in range(quantidade):
+print("\n-------------------------------------------------")
+print("Digite (1) para isento")
+print("Digite (2) pra informa o valor dos emolumentos")
+print("---------------------------------------------------\n")
+
+calcula = int(input(""))
+if calcula <= 0:
+    print("Opção invalida.")
+    calcula = int(input("informe uma opção valida: "))
+elif calcula > 2:
+    print("Opção invalida.")
+    calcula = int(input("informe uma opção valida: "))
+def opcao1():
+    global valor
+    valor = "Isento"
+def opcao2(): 
+    global valor
+    valor = str(input("Digite o valor dos emolumentos: "))
+opcoes = { 1:opcao1, 2:opcao2}
+opcoes.get(calcula)()
+
+print("\n-------------------------------")
+print("Digite (1) para Autenticidade ")
+print("Digite (2) para Semelhança\n")
+print("---------------------------------\n")
+recebe = int(input(""))
+if recebe <= 0:
+    print("Tipo digitado invalido.")
+    recebe = int(input("Digite um tipo valido: "))
+elif recebe > 2:
+    print("Tipo digitado invalido.")
+    recebe = int(input("Digite um tipo valido: "))
+def opcao1():
+    global tipo
+    tipo = "Autenticidade"
+def opcao2():
+    global tipo
+    tipo = "Semelhança"
+opcoes = { 1:opcao1, 2:opcao2}
+opcoes.get(recebe)()
+
+arraynome = []
+arraycpf = []
+
+i = 0
+quantidade = int(input("Informe a quantidade de partes: "))
+
+for i in range(quantidade):
+    nome = input("Digite seu nome: ")
+    cpf = input("digite seu cpf: ")
+
+    arraynome.append(nome)
+    arraycpf.append(cpf)
+print(len(arraynome))
+
+
+quantidadeEtiqueta = int(input('Quantidade de etiquetas? \n'))
+
+if quantidadeEtiqueta <= 0:
+    print("Quantidade invalida")
+    quantidadeEtiqueta = int(input('Quantidade de etiquetas? \n'))
    
-    print('________________Reconhecimento de Firma_________')
-    print('|   Reconheço por {} a(s) firmas de:       '.format(tipo2))       
-    print('|{}                                    '.format(pessoas))
-    print('|                                        ')
-    print('|Pará de Minas, {}                      '.format(dataat2))
-    print('|Valor dos Emolumentos: {}               '.format(isento2))
-    print('_________________________________________________')
+arquivo = open("Partes.txt", "w")
+for x in range(quantidadeEtiqueta):
+    arquivo.write('Reconhecimento de Firma\n')
+    arquivo.write('Reconheço por {} a(s) firmas de:\n'.format(tipo))
+    for x in range(len(arraynome)): 
+        arquivo.write('NOME: {}\n'.format(arraynome[x]))
+        arquivo.write('CPF: {}\n'.format(arraycpf[x]))
+    arquivo.write('{}, {}\n'.format(cidade,dataAtual))
+    arquivo.write('{} {}\n'.format(arrayEmol, valor))
+    arquivo.write('Quantidade de etiquetas: {}\n'.format(quantidadeEtiqueta))
+    arquivo.write('\n')
+arquivo.close()
+
+
+for x in range(quantidadeEtiqueta):
+    print('Reconhecimento de Firma')
+    print('Reconheço por {} a(s) firmas de:'.format(tipo))
+    for x in range(len(arraynome)):
+            print('NOME: {}'.format(arraynome[x]))
+            print('CPF: {}'.format(arraycpf[x]))    
+    print('{}, {}'.format(cidade,dataAtual))
+    print('{} {}'.format(arrayEmol, valor))
+    print('Quantidade de etiquetas: {}\n'.format(quantidadeEtiqueta))
+    
+import csv
+report = quantidade
+with open("salva.csv", 'a+', newline='') as salva:
+    escrever = csv.writer(salva)
+    for i in range(1):
+        for x in range(len(arraynome)):
+            escrever.writerow([arraynome[x], arraycpf[x], valor, cidade, dataAtual, quantidade])
+
